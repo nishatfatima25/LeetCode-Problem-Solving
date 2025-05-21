61e0e0d7f31bd9ecd264d595fee2117d796fd035
nishatfatima25 <its.nishatkhan004@gmail.com>
2025-05-21 23:35:52 +0530

    Create 263_Ugly_Number.cpp

---
+++ Easy/263_Ugly_Number.cpp
@@ -0,0 +1,21 @@
+// LeetCode Problem : 263. Ugly Number
+// Link : https://leetcode.com/problems/ugly-number/description/
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n==0){
+            return false;
+        }
+        while(n%2==0){
+            n = n/2;
+        }
+        while(n%3==0){
+            n = n/3;
+        }
+        while(n%5==0){
+            n = n/5;
+        }
+        return n==1;
+    }
+};
