3319392e2c0b660a147b77bd1f32da803a1d10dd
nishatfatima25 <its.nishatkhan004@gmail.com>
2025-05-22 00:05:49 +0530

    Create 520_Detect_Capital.cpp

---
+++ Easy/520_Detect_Capital.cpp
@@ -0,0 +1,29 @@
+// LeetCode Problem : 520. Detect Capital
+// Link : https://leetcode.com/problems/detect-capital/description/
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        int count = 0;
+        if(word.size() == 1){
+                return true;
+        }
+        for(int i=0; i< word.size(); i++){  
+            if(isupper(word[i])){
+                count++;
+            }
+        }
+
+        
+        if(count == 1 && isupper(word[0])){
+             return true;
+        } 
+
+        if(count ==0 || count == word.size()){
+            return true;
+        }
+
+        return false;
+    }
+};
