7815620e0ae84e75d0a9d3c1f90158065b2e08f2
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-05-26 20:35:47 +0530

    Update 118_Pascal's_Triangle.cpp

--- Easy/118_Pascal's_Triangle.cpp
+++ Easy/118_Pascal's_Triangle.cpp
@@ -1,6 +1,37 @@
 // LeetCode Problem : 118. Pascal's Triangle
 // Link : https://leetcode.com/problems/pascals-triangle/description/
 
+
+//method1
+class Solution {
+public:
+
+    vector<int> generateRows(int row){
+        int ans = 1;
+        vector<int> ansRow;
+        ansRow.push_back(ans);
+
+        for(int col = 1; col< row;col++){
+            ans = ans * (row-col);
+            ans = ans/col;
+            ansRow.push_back(ans);
+        }
+
+        return ansRow;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+
+        for(int i=1; i<=numRows; i++){
+            res.push_back(generateRows(i));
+        }
+
+        return res;
+    }
+};
+
+//method2
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
@@ -15,3 +46,5 @@ public:
         return result;
     }
 };
+
+
