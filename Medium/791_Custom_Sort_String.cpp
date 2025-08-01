8c6b0ce7caeca19c64e2474a5590c3e18da5bed4
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-08-01 21:12:04 +0530

    Create 791_Custom_Sort_String.cpp

---
+++ Medium/791_Custom_Sort_String.cpp
@@ -0,0 +1,26 @@
+// LeetCode Problem : 791. Custom Sort String
+// Link : https://leetcode.com/problems/custom-sort-string/description/
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        
+        vector<int> freq(26);
+
+        for(int i=0; i<s.size(); i++){
+            freq[s[i]-'a']++;
+        }
+
+        string ans = "";
+        for(int i=0; i<order.size(); i++){
+            ans.append(freq[order[i]-'a'], order[i]);
+            freq[order[i] - 'a']=0;
+        }
+
+        for(int i=0; i<26; i++){
+            ans.append(freq[i], 'a'+i);
+        }
+
+        return ans;
+    }
+};
