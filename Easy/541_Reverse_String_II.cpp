8448c663f77f730969b87b4977d2e7f7450963b2
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-09-23 20:09:29 +0530

    Create 541_Reverse_String_II.cpp

---
+++ Easy/541_Reverse_String_II.cpp
@@ -0,0 +1,19 @@
+// LeetCode Problem : 541. Reverse String II
+// Link : https://leetcode.com/problems/reverse-string-ii/description/
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for(int i=0; i<s.size(); i += 2*k){
+
+            if(i+k <= s.size()){
+                reverse(s.begin()+i, s.begin()+i+k);
+            }
+            else{
+                reverse(s.begin()+i, s.end());
+            }
+        }
+
+        return s;
+    }
+};
