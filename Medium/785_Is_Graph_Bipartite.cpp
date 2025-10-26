7418bb9a136ac290a949c9bf319cd57db92887c5
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-10-26 19:33:50 +0530

    Implement solution for checking bipartite graph

---
+++ Medium/785_Is_Graph_Bipartite.cpp
@@ -0,0 +1,36 @@
+// LeetCode Problem : 785. Is Graph Bipartite?
+// Link : https://leetcode.com/problems/is-graph-bipartite/description/
+
+class Solution {
+
+private:
+    bool dfs(int node, int col, vector<int> &color, vector<vector<int>>& graph)
+    {
+        color[node] = col;
+
+        for(auto it : graph[node]){
+            if(color[it] == -1){
+                if(dfs(it, 1 - col, color, graph) == false) return false;
+            }
+            else if(color[it] == col) return false;
+        }
+
+        return true;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> color(V, -1);
+
+        for(int i=0; i<V; i++) color[i] = -1;
+
+        for(int i=0; i<V; i++){
+            if(color[i] == -1){
+                if(dfs(i,0,color,graph) == false) return false;
+            }
+        }
+
+        return true;
+    }
+};
