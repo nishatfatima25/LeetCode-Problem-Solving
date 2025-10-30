c17d4d24ae7d07f3c309f6d7929a791f448b1726
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-10-30 21:17:28 +0530

    Add topological sort solution for safe nodes
    Implemented a topological sort solution to find eventual safe nodes in a directed graph.

--- Medium/802_Find_Eventual_Safe_States.cpp
+++ Medium/802_Find_Eventual_Safe_States.cpp
@@ -85,3 +85,53 @@ public:
         return ans;
     }
 };
+
+
+//solution 3 - topological sort
+
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> adj[V];
+        vector<int> adjRev[V];
+
+        for (int i = 0; i < V; i++) {
+            for (auto it : graph[i]) {
+                adj[i].push_back(it);        
+                adjRev[it].push_back(i);     
+            }
+        }
+
+
+        vector<int> indegree(V,0);
+        queue<int> q;
+
+        for(int i=0; i<V; i++){
+            for(auto it : adjRev[i]){
+                indegree[it]++;
+            }
+        }
+
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it : adjRev[node]){
+                indegree[it]--;
+
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
