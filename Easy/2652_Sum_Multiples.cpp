680d1951c875b9f6fe256d843cf7d7f541465ff1
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-11-01 17:17:22 +0530

    Create 2652_Sum_Multiples.cpp

---
+++ Easy/2652_Sum_Multiples.cpp
@@ -0,0 +1,17 @@
+// LeetCode Problem : 2652. Sum Multiples
+// Link : https://leetcode.com/problems/sum-multiples/description/
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum =0;
+
+        for(int i=1; i<=n; i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+};
