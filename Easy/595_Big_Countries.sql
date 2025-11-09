358eb210337a75c39ffe572d17691a6fdd824e3e
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-11-09 16:52:28 +0530

    Create 595_Big_Countries.sql

---
+++ Easy/595_Big_Countries.sql
@@ -0,0 +1,7 @@
+// LeetCode Problem : 595. Big Countries
+// Link : https://leetcode.com/problems/big-countries/description/
+
+
+select name,population,area
+from World
+where area >= 3000000 or population >= 25000000
