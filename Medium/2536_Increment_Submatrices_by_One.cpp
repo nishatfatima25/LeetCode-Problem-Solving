109b6ebbd134160d6d2b48716b744421e296aa0c
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-11-14 18:24:06 +0530

    Implement Increment Submatrices by One solution

---
+++ Medium/2536_Increment_Submatrices_by_One.cpp
@@ -0,0 +1,35 @@
+// LeetCode Problem : 2536. Increment Submatrices by One
+// Link : https://leetcode.com/problems/increment-submatrices-by-one/description/
+
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+
+        vector<vector<int>> diff(n + 1, vector<int>(n + 1, 0));
+
+        for (auto &q : queries) {
+            int r1 = q[0], c1 = q[1];
+            int r2 = q[2], c2 = q[3];
+
+            diff[r1][c1] += 1;
+            diff[r1][c2 + 1] -= 1;
+            diff[r2 + 1][c1] -= 1;
+            diff[r2 + 1][c2 + 1] += 1;
+        }
+
+        vector<vector<int>> result(n, vector<int>(n, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                
+                int top = (i > 0 ? result[i - 1][j] : 0);
+                int left = (j > 0 ? result[i][j - 1] : 0);
+                int diag = (i > 0 && j > 0 ? result[i - 1][j - 1] : 0);
+
+                result[i][j] = diff[i][j] + top + left - diag;
+            }
+        }
+
+        return result;
+    }
+};
