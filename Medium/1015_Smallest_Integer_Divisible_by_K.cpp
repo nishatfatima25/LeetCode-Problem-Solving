179d1cfea83ca2ad5c561b82772576268b87fad1
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-11-25 14:12:08 +0530

    Add solution for smallest integer divisible by K

---
+++ Medium/1015_Smallest_Integer_Divisible_by_K.cpp
@@ -0,0 +1,16 @@
+// LeetCode Problem : 1015. Smallest Integer Divisible by K
+// Link : https://leetcode.com/problems/smallest-integer-divisible-by-k/description/
+
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+
+        int rem =0;
+        for(int i=1; i<=k; i++){
+            rem = (rem*10+1)%k;
+            if(rem == 0) return i;
+        }
+
+        return -1;
+    }
+};
