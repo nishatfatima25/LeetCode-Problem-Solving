2c3517b1ba56d5462a0da4faf6621a112fa0eb82
Nishat Fatima <its.nishatkhan004@gmail.com>
2025-12-08 14:38:05 +0530

    Add solution for LeetCode problem 1925
    Implement solution for counting square sum triples.

---
+++ Easy/1925_Count_Square_Sum_Triples.cpp
@@ -0,0 +1,25 @@
+// LeetCode Problem : 1925. Count Square Sum Triples
+// Link : https://leetcode.com/problems/count-square-sum-triples/description/
+
+class Solution {
+public:
+    int countTriples(int n) {
+        vector<bool> isSquare(n*n+1, false);
+        for(int i=1; i<=n; i++){
+            isSquare[i*i] = true;
+        }
+        
+
+        int count =0;
+
+        for(int a =1; a<=n; a++){
+            for(int b=1; b<=n; b++){
+                int sum = a*a + b*b;
+
+                if(sum <= n*n && isSquare[sum]) count++;
+            }
+        }
+        
+        return count;
+    }
+};
