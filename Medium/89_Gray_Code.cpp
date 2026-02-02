602e6dac178ede84ff4c17cedcdb94b922fc5ded
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-02-02 19:07:56 +0530

    Improve formatting of grayCode function
    Refactor grayCode function for better readability.

--- Medium/89_Gray_Code.cpp
+++ Medium/89_Gray_Code.cpp
@@ -1,1 +1,14 @@
-class Solution { public: vector<int> grayCode(int n) { int x = 1 <<n; vector<int> ans(x); for(int i=0; i<x; i++){ ans[i] = i^(i>>1); } return ans; } };
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        
+        int x = 1 <<n;
+        vector<int> ans(x);
+
+        for(int i=0; i<x; i++){
+            ans[i] = i^(i>>1);
+        }
+
+        return ans;
+    }
+};
