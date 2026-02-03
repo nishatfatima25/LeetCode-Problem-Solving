659d23fa5b89bd7d03e85ecc1f70fbb86a5e1fb6
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-02-03 20:15:09 +0530

    Add Solution class for LeetCode Problem 3637

---
+++ Easy/3637_Trionic_Array_I.cpp
@@ -0,0 +1,24 @@
+// LeetCode Problem : 3637. Trionic Array I
+// Link : https://leetcode.com/problems/trionic-array-i/description/
+
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n<4) return false;
+        
+        int i=0;
+        while(i<n-1 && nums[i] < nums[i+1]) i++;
+
+        if(i==0 || i==n-1) return false;
+        int p = i;
+
+        while(i<n-1 && nums[i]>nums[i+1])i++;
+
+        if(i==p || i==n-1) return false;
+        while(i<n-1 && nums[i] < nums[i+1]) i++;
+
+        return i == n-1;
+    }
+};
