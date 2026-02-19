db1a954cf655de76b10f5be5db59f1c92dd0074b
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-02-19 22:23:57 +0530

    Update 19_Remove_Nth_Node_From_End_of_List.cpp

--- Medium/19_Remove_Nth_Node_From_End_of_List.cpp
+++ Medium/19_Remove_Nth_Node_From_End_of_List.cpp
@@ -11,6 +11,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//Brute Force
 class Solution {
 public:
 
@@ -55,5 +57,27 @@ public:
 
         ListNode* original = rev(newHead);
         return original;
+    }
+};
+
+//Optimal Approach
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        for(int i=0; i<n; i++) fast = fast->next;
+        if(fast == NULL) return head->next; //edge case
+
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+
+        delete delNode;
+        return head;
     }
 };
