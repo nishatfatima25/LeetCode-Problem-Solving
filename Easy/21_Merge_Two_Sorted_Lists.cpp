0220c97b26a22c41d678e4c5fefc607a89347f54
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-02-21 15:47:42 +0530

    Update 21_Merge_Two_Sorted_Lists.cpp

--- Easy/21_Merge_Two_Sorted_Lists.cpp
+++ Easy/21_Merge_Two_Sorted_Lists.cpp
@@ -49,3 +49,33 @@ public:
         return newhead;
     }
 };
+
+
+// Optimal
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* t1 = list1;
+        ListNode* t2 = list2;
+        ListNode* dNode = new ListNode(-1);
+        ListNode* temp = dNode;
+
+        while(t1 != NULL && t2 != NULL){
+            if(t1->val < t2->val){
+                temp->next = t1;
+                temp = t1;
+                t1 = t1->next;
+            }
+            else{
+                temp->next = t2;
+                temp = t2;
+                t2 = t2->next;
+            }
+        }
+
+        if(t1) temp->next = t1;
+        else temp->next = t2;
+
+        return dNode->next;
+    }
+};
