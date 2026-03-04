e2e17b3a637a784d2f52327350f438ac4cc93db9
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-03-04 20:47:20 +0530

    Create 1582_Special_Positions_in_a_Binary_Matrix.cpp

---
+++ Easy/1582_Special_Positions_in_a_Binary_Matrix.cpp
@@ -0,0 +1,24 @@
+// LeetCode Problem : 1582. Special Positions in a Binary Matrix
+// Link : https://leetcode.com/problems/special-positions-in-a-binary-matrix/description/
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<int> row(n,0), col(m,0);
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 1) row[i]++, col[j]++;
+            }
+        }
+        int count =0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1) count++;
+            }
+        }
+        return count;
+    }
+};
