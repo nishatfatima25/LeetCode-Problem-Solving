a6f86725a3da6e8764e8662c5de8f867f88bbb6e
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-03-24 23:42:47 +0530

    Create 3875_Construct_Uniform_Parity_Array_I.cpp

---
+++ Easy/3875_Construct_Uniform_Parity_Array_I.cpp
@@ -0,0 +1,9 @@
+// LeetCode Problem : 3875. Construct Uniform Parity Array I
+// Link : https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
