f22e388e2a9ce83bbe858629547afffb50866ae6
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-03-30 20:53:39 +0530

    Create 2839_Check_if_Strings_Can_be_Made_Equal_With_Operation_I.cpp

---
+++ Easy/2839_Check_if_Strings_Can_be_Made_Equal_With_Operation_I.cpp
@@ -0,0 +1,28 @@
+// LeetCode Problem : 2839. Check if Strings Can be Made Equal With Operation I
+// Link : https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/?envType=daily-question&envId=2026-03-29
+
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        string even1 = "",even2 = "";
+        string odd1 = "", odd2 = "";
+
+        for(int i=0; i<s1.size(); i++){
+            if(i%2 == 0){
+                even1 += s1[i];
+                even2 += s2[i];
+            }
+            else{
+                odd1 += s1[i];
+                odd2 += s2[i];
+            }
+        }
+
+        sort(even1.begin(),even1.end());
+        sort(odd1.begin(),odd1.end());
+        sort(even2.begin(),even2.end());
+        sort(odd2.begin(),odd2.end());
+
+        return even1 == even2 && odd1 == odd2;
+    }
+};
