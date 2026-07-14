04f7043aebd5ce7d97e415d51830e3276ec746c6
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-07-14 23:59:22 +0530

    Update 13_Roman_to_Integer.cpp

--- Easy/13_Roman_to_Integer.cpp
+++ Easy/13_Roman_to_Integer.cpp
@@ -38,3 +38,21 @@ public:
         return sum;
     }
 };
+
+
+
+// Method - 2
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<int,int> mpp = {
+            {'I',1},{'V',5},{'X',10}, {'L',50}, {'C',100}, {'D',500},{'M',1000} };
+        
+        int ans = 0;
+        for(int i=0; i<s.size()-1; i++){
+            if(mpp[s[i]] < mpp[s[i+1]]) ans -= mpp[s[i]];
+            else ans += mpp[s[i]];
+        }
+        return ans + mpp[s.back()];
+    }
+};
