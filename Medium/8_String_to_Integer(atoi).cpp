5c8720ce392647c134c1fa53ff8e5b6a0b5ce1e4
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-07-20 21:56:44 +0530

    Update 8_String_to_Integer(atoi).cpp

--- Medium/8_String_to_Integer(atoi).cpp
+++ Medium/8_String_to_Integer(atoi).cpp
@@ -31,3 +31,30 @@ public:
         return ans;
     }
 };
+
+// By Recursion
+class Solution {
+public:
+    int solve(string &s, int i, long long num, int sign){
+        if(i >= s.size() || !isdigit(s[i])) return (int)(sign*num);
+        num = num*10 + (s[i]-'0');
+
+        if(sign*num <= INT_MIN) return INT_MIN;
+        if(sign*num >= INT_MAX) return INT_MAX;
+
+        return solve(s,i+1,num,sign);
+
+    }
+    int myAtoi(string s) {
+        int i=0;
+        while(i<s.size() && s[i] == ' ') i++;
+
+        int sign = 1;
+        if(i<s.size() && (s[i] == '+' || s[i] == '-')){
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+
+        return solve(s,i,0,sign);
+    }
+};
