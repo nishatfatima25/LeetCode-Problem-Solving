2e877024cef52e17dbf791f5fde9459d435d2f93
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-07-24 21:06:38 +0530

    Update 90_Subsets_II.cpp

--- Medium/90_Subsets_II.cpp
+++ Medium/90_Subsets_II.cpp
@@ -29,3 +29,33 @@ public:
         return res;
     }
 };
+
+// Method -2 
+class Solution {
+public:
+
+    void solve(vector<int> &input, vector<int> &output, vector<vector<int>> &ans, int idx){
+        if(idx == input.size()){
+            ans.push_back(output);
+            return;
+        }
+
+        output.push_back(input[idx]);
+        solve(input,output,ans,idx+1);
+        output.pop_back();
+
+        int nextIdx = idx+1;
+        while(nextIdx < input.size() && (input[nextIdx] == input[nextIdx-1])) nextIdx++;
+
+        solve(input,output,ans,nextIdx);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> output;
+        solve(nums,output,ans,0);
+
+        return ans;
+    }
+};
