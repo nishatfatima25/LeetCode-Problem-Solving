8fb78c79502e6fd67f7d83508e69740cfb11321d
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-08-19 20:50:33 +0530

    Create 63_Unique_Paths_II.cpp

---
+++ Medium/63_Unique_Paths_II.cpp
@@ -0,0 +1,26 @@
+// LeetCode Problem : 63. Unique Paths II
+// Link : https://leetcode.com/problems/unique-paths-ii/description/
+
+class Solution {
+public:
+
+    int solve(int i, int j, vector<vector<int>> &dp, vector<vector<int>> &obstacleGrid){
+        if(i<0 || j<0 || obstacleGrid[i][j] == 1) return 0;
+        else if(i==0 && j==0) return 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = solve(i-1,j,dp,obstacleGrid);
+        int left = solve(i,j-1,dp,obstacleGrid);
+
+        return dp[i][j] = up + left;
+    }
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp,obstacleGrid);
+    }
+};
