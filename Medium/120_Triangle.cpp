03bf44168dff2bbb8999c22ebbc9899a7d80ed96
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-08-20 13:14:07 +0530

    Update 120_Triangle.cpp

--- Medium/120_Triangle.cpp
+++ Medium/120_Triangle.cpp
@@ -14,3 +14,26 @@ public:
         return triangle[0][0];
     }
 };
+
+
+// Method-2
+class Solution {
+public:
+
+    int solve(int i, int j, vector<vector<int>> &triangle, vector<vector<int>> &dp){
+        int n = triangle.size();
+        if(i== n-1) return triangle[i][j];
+        if(dp[i][j] != INT_MAX) return dp[i][j];
+        
+        int down = triangle[i][j] + solve(i+1,j,triangle,dp);
+        int diag = triangle[i][j] + solve(i+1,j+1,triangle,dp);
+
+        return dp[i][j] = min(down,diag);
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        return solve(0,0,triangle,dp);
+    }
+};
